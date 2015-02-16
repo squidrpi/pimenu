a665331a9776ffb9084191a577742f99d9d44d45
squidrpi <squidrpi@users.noreply.github.com>
2015-02-16 13:08:36 +0000

    Updates

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+#Only used for Pi Store App
 
-cd /usr/local/bin/indiecity/InstalledApps/pimenu/Full
+cd /usr/local/bin/indiecity/InstalledApps/pi-menu/Full
 chmod 777 ./pimenu ./pimenu.cfg ICON*
